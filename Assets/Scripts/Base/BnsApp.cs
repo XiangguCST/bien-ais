ea147c8224828f808c942ea43f4939fd6a6cf4d1
YanF <744539997@qq.com>
2023-06-24 12:18:28 +0800

    导入刺客素材

---
+++ Assets/Scripts/Base/BnsApp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YFrameWork;
+
+// 游戏应用类
+public class BnsApp : BaseApp<BnsApp>
+{
+    private BnsApp() { }
+
+    protected override void InitApp()
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 角色
+public class Character
+{
+    public int hp;
+    public int atk;
+    public float speed;
+}
+
+public class Player : Character
+{
+    public int mp;
+}
+
