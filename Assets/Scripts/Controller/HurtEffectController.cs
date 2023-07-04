4b5557bd230cc7eda829b3ca63f0c4d47bf3056d
YanF <744539997@qq.com>
2023-07-04 15:05:07 +0800

    修正面板按钮失灵问题，优化受到攻击效果

--- Assets/Scripts/Controller/HurtEffectController.cs
+++ Assets/Scripts/Controller/HurtEffectController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class HurtEffectController
 {
-    // 显示受伤效果
     static public void ShowHurtEffect(Character other)
     {
         CoroutineRunner.StartCoroutine(FlashRoutine(other));
@@ -11,33 +10,40 @@ public class HurtEffectController
 
     static IEnumerator FlashRoutine(Character other)
     {
-        var  mpb = new MaterialPropertyBlock();
+        var mpb = new MaterialPropertyBlock();
         var meshRenderer = other.GetComponentInChildren<MeshRenderer>();
         meshRenderer.GetPropertyBlock(mpb);
 
         if (flashCount < 0) flashCount = DefaultFlashCount;
         Color oriColor = other._defaultColor;
 
+        float dynamicInterval = interval; // 初始闪烁间隔
         for (int i = 0; i < flashCount; i++)
         {
-            for (float t = 0; t <= 1; t += Time.deltaTime / interval)
+            // 变红
+            for (float t = 0; t <= 1; t += Time.deltaTime / dynamicInterval)
             {
                 Color currentColor = Color.Lerp(oriColor, flashColor, t);
                 mpb.SetColor(fillColorProperty, currentColor);
-                if(meshRenderer != null)
+                if (meshRenderer != null)
                     meshRenderer.SetPropertyBlock(mpb);
                 yield return null;
             }
 
-            for (float t = 0; t <= 1; t += Time.deltaTime / interval)
+            // 恢复颜色
+            for (float t = 0; t <= 1; t += Time.deltaTime / dynamicInterval)
             {
                 Color currentColor = Color.Lerp(flashColor, oriColor, t);
                 mpb.SetColor(fillColorProperty, currentColor);
-                if(meshRenderer != null)
+                if (meshRenderer != null)
                     meshRenderer.SetPropertyBlock(mpb);
                 yield return null;
             }
+
+            // 每个闪烁周期结束后，增加间隔时间，使闪烁频率减少
+            dynamicInterval *= 1.5f;
         }
+
         mpb.SetColor(fillColorProperty, oriColor);
         if (meshRenderer != null)
             meshRenderer.SetPropertyBlock(mpb);
@@ -46,9 +52,9 @@ public class HurtEffectController
 
     public static string fillColorProperty = "_FillColor";
 
-    static int DefaultFlashCount = 2;
+    static int DefaultFlashCount = 3;
 
     static public int flashCount = DefaultFlashCount;
     static public Color flashColor = Color.red;
-    static public float interval = 1f / 10f;
+    static public float interval = 1f / 60f;
 }
